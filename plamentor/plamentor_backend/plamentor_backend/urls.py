49fe644229e2aeba1b818a07ab9b43bee3f17d6a
fadhillahrizki0701 <fadhillahrizki0701@gmail.com>
2024-10-14 13:44:15 +0700

    refactor: reset project structures with Python virtual environment

--- plamentor/plamentor_backend/plamentor_backend/urls.py
+++ plamentor/plamentor_backend/plamentor_backend/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for platore project.
+URL configuration for plamentor_backend project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
--- plamentor/plamentor_backend/plamentor_backend/wsgi.py
+++ plamentor/plamentor_backend/plamentor_backend/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for platore project.
+WSGI config for plamentor_backend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'platore.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'plamentor_backend.settings')
 
 application = get_wsgi_application()
