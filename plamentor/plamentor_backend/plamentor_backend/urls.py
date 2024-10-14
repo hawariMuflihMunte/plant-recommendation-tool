de65ea6b0ab0f7b5161f01ed2c50826baaddb952
fadhillahrizki0701 <fadhillahrizki0701@gmail.com>
2024-10-14 13:58:09 +0700

    feat(urls): add `` or `\` path

--- plamentor/plamentor_backend/plamentor_backend/urls.py
+++ plamentor/plamentor_backend/plamentor_backend/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from core.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', ReactView.as_view(), name="Something"),
 ]
