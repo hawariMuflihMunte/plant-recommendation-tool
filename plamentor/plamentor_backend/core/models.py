71e7dd2ad431d3b6c9283096d674b87aeb64742b
fadhillahrizki0701 <fadhillahrizki0701@gmail.com>
2024-10-14 13:48:43 +0700

    feat(serializer): create `ReactSerializer`

--- plamentor/plamentor_backend/core/models.py
+++ plamentor/plamentor_backend/core/models.py
@@ -3,3 +3,5 @@ from django.db import models
 class React(models.Model):
     name = models.CharField(max_length=50)
     detail = models.CharField(max_length=500)
+
+
---
+++ plamentor/plamentor_backend/core/serializer.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from . models import *
+
+class ReactSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = React
+        fields = [
+            'name',
+            'detail'
+        ]
