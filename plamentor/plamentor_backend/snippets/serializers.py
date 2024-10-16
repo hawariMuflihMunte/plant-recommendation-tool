57c78b7ade4e59490ddf4e8708899bbca6d70377
fadhillahrizki0701 <fadhillahrizki0701@gmail.com>
2024-10-17 01:16:15 +0700

    feat: set up Meta in SnippetSerializer

--- plamentor/plamentor_backend/snippets/serializers.py
+++ plamentor/plamentor_backend/snippets/serializers.py
@@ -27,3 +27,14 @@ class SnippetSerializer(serializers.Serializer):
         instance.style = validated_data.get('style', instance.style)
         instance.save()
         return instance
+
+    class Meta:
+        model = Snippet
+        fields = [
+            'id',
+            'title',
+            'code',
+            'linenos',
+            'language',
+            'style',
+        ]
