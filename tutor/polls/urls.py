42ff56b33a193d15a8bb45d7c1292e0134ed12b2
fadhillahrizki0701 <fadhillahrizki0701@gmail.com>
2024-10-17 14:48:15 +0700

    refactor: update detail path url

--- tutor/polls/urls.py
+++ tutor/polls/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', view=views.index, name='index'),
-    path('<int:question_id>/', views.detail, name='detail'),
+    # the 'name' value as called by the {% url %} template tag
+    path('details/<int:question_id>/', views.detail, name='detail'),
     path('<int:question_id>/results/', views.results, name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
