56a9ab52128010c7f7804b10d825ab5c30ef20d2
fadhillahrizki0701 <fadhillahrizki0701@gmail.com>
2024-10-18 12:04:03 +0700

    test: add 6 tests - create_question - QuestionIndexViewTests/test_no_questions - QuestionIndexViewTests/test_past_question - QuestionIndexViewTests/test_future_question - QuestionIndexViewTests/test_future_question_and_past_question - QuestionIndexViewTests/test_two_past_question

--- tutor/polls/views.py
+++ tutor/polls/views.py
@@ -1,8 +1,9 @@
 from django.db.models import F
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 
@@ -11,8 +12,11 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """
+        Return the last five published questions
+        (not including those set to be published in the future)
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
