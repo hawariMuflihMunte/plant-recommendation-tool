c6ce242c11df69567d5c3599c2f9ac2b919cca3f
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2024-10-24 11:20:56 +0700

    feat: set up login logic

--- plamentor/core/views.py
+++ plamentor/core/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.shortcuts import redirect
 
 def index(request):
     return render(request, 'core/pages/index.html')
 
 def login_view(request):
-    return render(request, 'core/auth/login.html')
+    if request.method == 'GET':
+        return render(request, 'core/auth/login.html')
+    
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Successfully loggen in.')
+            return redirect('core:home')
+        else:
+            messages.error(request, 'Invalid username or password')
+            return redirect('core:login')
+
+    return redirect('core:login')
