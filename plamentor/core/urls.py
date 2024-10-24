967b98692530142c218dcbcf4dd8ebe587f26429
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2024-10-24 10:55:33 +0700

    feat: set up login path in navbar

--- plamentor/core/urls.py
+++ plamentor/core/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'core'
 urlpatterns = [
     path('', view=views.index, name='home'),
-    path('login/', view=views.login_view, name='auth_login'),
+    path('login/', view=views.login_view, name='login'),
 ]
